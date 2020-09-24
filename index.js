bf6105f059bf9df13ac2a97d98cfd9cc85af21a6
dcoa <dcoa@live.com>
2020-09-23 21:48:51 -0500

    Return array with links only one md file

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = () => {
-  // ...
+const getLinks = require('./lib/evFile.js');
+
+const mdLinks = (path, options) => {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(getLinks(path))
+    } catch (e) {
+      reject(e.message)
+    }
+  })
 };
+
+mdLinks('./README.md').then(console.log).catch(console.error)
+module.exports = mdLinks;
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,42 @@
 const mdLinks = require('../');
-
+const evFile = require('../lib/evFile.js');
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it.only('should...', () => {
+    console.log(mdLinks('./README.md'));
   });
 
+  it('should...', () => {
+    expect(mdLinks('./git')).toBe('The file or directory does not exist');
+  });
 });
+
+describe('evPathType', () => {
+
+  it('should return the path to the md file', () => {
+    const absolutePath = '/Users/usuario/Desktop/progbasic/Laboratoria/bog001-md-links/README.md'
+    expect(evFile.evPathType()).toBe(absolutePath);
+  });
+  it('should return no es', () => {
+    expect(evFile.evPathType('./index.js')).toBe("We haven't identified a markdown file.");
+  });
+  it('should return the directory path relative to cwd', () => {
+    const relativePath = './'
+    expect(evFile.evPathType('./')).toBe(relativePath);
+  });
+});
+
+
+/*describe('pathExist', () => {
+
+  it('should return true', () => {
+    expect(evFile.pathExist('./README.md')).toBeTruthy();
+  });
+  it('should return true', () => {
+    expect(evFile.pathExist('./index.js')).toBeTruthy();
+  });
+  it('should return false', () => {
+    expect(evFile.pathExist('./log')).toBe('The file or directory does not exist');
+  });
+});*/
